18cb5d39e3b5235108012a2db72b1b60f158fc53
Victor Wong <vj1901@hotmail.com>
2017-01-12 21:04:45 +0000

    completed 'filter it out', added 'filter array'

--- answers.js
+++ answers.js
@@ -1,3 +1,4 @@
+// Only the pos
 // Write a function called printPositives that takes an array and uses the forEach 
 // method to print only the positive numbers.
 
@@ -13,10 +14,10 @@ Array.prototype.printPositives = function (item) {
 
 someArray.printPositives(); //testing
 
+// Get positive
 // Similar to the previous exercise, write a function called getPositives that 
 // takes an array and uses the filter method to return a new array with only the positive numbers.
 
-// my solution
 var someArrayTwo = [1,-5,-6,423,-873,45,-8764,87,9,3,2,-45,5,0,-8,44];
 
 function getPositives(item) {
@@ -24,3 +25,18 @@ function getPositives(item) {
 }
 
 console.log(someArrayTwo.filter(getPositives)); // testing
+
+// Filter it out
+// Re-do exercise 1 by first filtering the input array, and then printing the numbers 
+// from the filtered array. Your code will look something like: return arr.filter(...).forEach(...).
+
+var someArrayThree = [0, 1,-5,-6, 42, 3,-873,45,-87, 64 , -101, 87, 9, 3, 2, -45,5,0,-8,44];
+
+return someArrayThree.filter((x) => x >= 0).forEach(x => console.log(x));
+
+// Filter Array
+// Write a function called filterArray that takes a callback function and an array as arguments. 
+// Your filterArray function should return a new array that contains only the elements where the 
+// callback function returns true.
+// NOTE: This is a trick question. The answer is a one-liner and you are allowed to use array.filter :)
+
