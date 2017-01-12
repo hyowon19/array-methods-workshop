5a4fe236cf6ae6af853b30502e7543460ab78056
Victor Wong <vj1901@hotmail.com>
2017-01-12 20:13:17 +0000

    added getPositives function

--- answers.js
+++ answers.js
@@ -1,33 +1,26 @@
+// Write a function called printPositives that takes an array and uses the forEach 
+// method to print only the positive numbers.
+
 var someArray = [1,-5,-6,423,-873,9,3,2,-45,5,0,-8];
 
 Array.prototype.printPositives = function (item) {
     this.forEach(function(item) {
-        if(item > 0) {
+        if(item >= 0) {
             console.log(item)
         }
     })
 }
 
-someArray.printPositives();
+someArray.printPositives(); //testing
 
+// Similar to the previous exercise, write a function called getPositives that 
+// takes an array and uses the filter method to return a new array with only the positive numbers.
 
-//  printPositives.someArray;
-// var students = ["alice", "bob", "charles", "darwin"];
+// my solution
+var someArrayTwo = [1,-5,-6,423,-873,45,-8764,87,9,3,2,-45,5,0,-8,44];
 
-// var filteredStudents = [];
+function getPositives(item) {
+    return item >= 0;
+}
 
-// students.forEach(function(item) {
-//     if(item[0] !== "a") {  //item.length > 3   //
-//       filteredStudents.push(item);
-//     }
-// });
-
-
-// Array.prototype.printPositives = function (arr) {
-//     var filtPositive = [];
-//     this.forEach(function(item)) {
-//         if(arr(item) > 0) {
-            
-//         }
-//     }
-// }
+console.log(someArrayTwo.filter(getPositives)); // testing
